644c4d3b3768905364c8a9ba41e3db2717880e6f
prussia1891 <prussia1891@gmail.com>
2025-02-10 12:06:34 -0800

    make some changes

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -3,22 +3,19 @@ using UnityEngine.Events;
 public class BallController : MonoBehaviour
 {
     [SerializeField] private float force = 1f;
-    // name booleans like a question
-    private bool isBallLaunched;
+    [SerializeField] private InputManager inputManager;
     private Rigidbody ballRB;
-    private InputManager inputManager;
     void Start()
     {
+        //Grabbing a reference to RigidBody
         ballRB = GetComponent<Rigidbody>();
+        // Add a listener to the OnSpacePressed event.
+        // When the space key is pressed the
+        // LaunchBall method will be called.
         inputManager.OnSpacePressed.AddListener(LaunchBall);
     }
     private void LaunchBall()
     {
-        // now your if check can be framed like a sentence
-        // "if ball is launched, then simply return and do nothing"
-        if (isBallLaunched) return;
-        // "now that the ball is not launched, set it to true and launch the ball"
-        isBallLaunched = true;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
