b932c3c51c69c47bc1edf24d6b1ca29b03983946
prussia1891 <prussia1891@gmail.com>
2025-02-10 15:48:47 -0800

    gutters implementation

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,7 +4,10 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] private float force = 1f;
     [SerializeField] private InputManager inputManager;
+    // name booleans like a question
+    private bool isBallLaunched;
     private Rigidbody ballRB;
+
     void Start()
     {
         //Grabbing a reference to RigidBody
@@ -16,6 +19,11 @@ public class BallController : MonoBehaviour
     }
     private void LaunchBall()
     {
+        // now your if check can be framed like a sentence
+        // "if ball is launched, then simply return and do nothing"
+        if (isBallLaunched) return;
+        // "now that the ball is not launched, set it to true and launch the ball"
+        isBallLaunched = true;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
