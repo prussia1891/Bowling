5f95d2aafbad53269dbacccc5602b48ee2836b34
prussia1891 <prussia1891@gmail.com>
2025-02-10 19:42:34 -0800

    Launch Indicator

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,6 +5,7 @@ public class BallController : MonoBehaviour
     [SerializeField] private float force = 1f;
     [SerializeField] private InputManager inputManager;
     [SerializeField] private Transform ballAnchor;
+    [SerializeField] private Transform launchIndicator;
 
     // name booleans like a question
     private bool isBallLaunched;
@@ -31,7 +32,7 @@ public class BallController : MonoBehaviour
         isBallLaunched = true;
         transform.parent = null;
         ballRB.isKinematic = false;
-        // this sets the object to the outermost layer of the hierarchy
-        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+        ballRB.AddForce(launchIndicator.forward * force, ForceMode.Impulse);
+        launchIndicator.gameObject.SetActive(false);
     }
 }
